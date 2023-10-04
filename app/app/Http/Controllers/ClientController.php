2f9815995fb5e835906534e26b9ba5c79447e225
Manel <manel.aguilera91@gmail.com>
2023-10-05 00:04:07 +0200

    add first test to validate fields

--- app/app/Http/Controllers/ClientController.php
+++ app/app/Http/Controllers/ClientController.php
@@ -2,19 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Helpers\ValidatorRequest;
+use App\Http\Services\ClientService;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class ClientController extends Controller
 {
+    private ClientService $clientService;
+    private ValidatorRequest $validatorRequest;
+
     public function __construct()
     {
+        $this->clientService = new ClientService();
+        $this->validatorRequest = new ValidatorRequest();
     }
 
     public function store(Request $request): JsonResponse
     {
-        return response()->json([
-            'data' => 'hola'
-        ], 200);
+        try {
+            
+            $this->validatorRequest->validate($request);
+            
+           // $client = $this->clientService->store($request);
+
+            return response()->json('hola', 201);
+        } catch (BadRequestHttpException $e) {
+            return response()->json(['error' => $e->getMessage()], $e->getStatusCode());
+        }
     }
 }
